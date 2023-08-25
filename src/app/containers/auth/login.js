92cd13ecf0268abe9b5fe149a69891e4984ecde4
arun6493 <arun.sharma@itechnolabs.tech>
2023-08-25 18:25:26 +0530

    Login page

--- src/app/containers/auth/login.js
+++ src/app/containers/auth/login.js
@@ -25,9 +25,41 @@ const Login = (props) => {
                             </div>
                         </div>
                         <div className="col-lg-6">
-                            <div className="logRight">
+                            <div className="logRight mt-5">
                                 <div className="container">
-                                    <h3>Don’t have an account?</h3>
+                                    <div className="flexBox justify-content-end">
+                                        <h3 className="smallSubheading">Don’t have an account?</h3>
+                                        <div class="form-group">
+                                            <label class="toggle">
+                                                <input type="checkbox" />
+                                                <span class="slider"></span>
+                                                <span class="labels" data-on="Login" data-off=" Sign Up"></span>
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div className="logForm">
+                                        <h6 className="mainHeading mb-3">Login</h6>
+                                        <p className="subHeading">Enter your login details below</p>
+                                        <div class="input-container mt-5">
+                                            <input type="text" class="border-input" placeholder="bangura@serveitlocal.com" />
+                                            <label class="border-label">Email</label>
+                                        </div>
+                                        <div class="input-container mt-5">
+                                            <input type="password" class="border-input"/>
+                                            <label class="border-label">Password</label>
+                                        </div>
+                                        <div className="flexBox justify-content-between mt-5">
+                                            <div class="custom-checkbox">
+                                                <input type="checkbox" id="checkbox" class="checkbox-input"/>
+                                                    <label for="checkbox" class="checkbox-label smallSubheading">Keep me Logged in</label>
+                                            </div>
+                                            <p className="coloredSmallSubheading m-0">Forgot Password?</p>
+                                        </div>
+                                        <div className="buttonBox mt-5">
+                                            <button type="submit" role="button" className="smallBtn"> Login</button>
+                                        </div>
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
