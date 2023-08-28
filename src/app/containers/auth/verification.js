cd53a10467e4390c9a085f44c289504c10c74f53
arun6493 <arun.sharma@itechnolabs.tech>
2023-08-28 17:51:05 +0530

    verify page

--- src/app/containers/auth/verification.js
+++ src/app/containers/auth/verification.js
@@ -1,46 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import { useDispatch } from 'react-redux';
-import { MuiOtpInput } from 'mui-one-time-password-input'
-import { userOtp } from '../../../redux/slices/auth';
-import { useAuthSelector } from '../../../redux/selector/auth';
+import React, { useState } from 'react';
+import * as Images from "../../../utilities/images";
+import OtpInput from 'react-otp-input';
+
 
 const Verification = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const location = useLocation();
-    const authSelector = useAuthSelector()
-    let user_id = location?.state?.id;
     const [otp, setOtp] = useState('');
-
-    //onchange otp
-    const handleChangePin = (newValue) => {
-        console.log(newValue,"newValue");
-        setOtp(newValue)
-    }
-
-    //form submit
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (!otp) {
-            toast.error("Please enter OTP");
-            return;
-        }
-        let params = {
-            user_id: user_id,
-            otp: parseInt(otp),
-            status: 1
-            // otp: otp,
-        }
-    }
-    useEffect(() => {
-        document.title = "Verification";
-    }, []);
-
     return (
         <>
-        
+            <div className="Login">
+                <div className="container-fluid">
+                    <div className="row align-items-center">
+                        <div className="col-lg-6">
+                            <div className="logleft">
+                                <figure>
+                                    <img src={Images.Logo} alt="logo" className="img-fluid logo" />
+                                </figure>
+                                <figure className="ChefMain">
+                                    <img src={Images.Chef} alt="Chef" className="img-fluid Chef" />
+                                </figure>
+                                <figure className="foodBox">
+                                    <img src={Images.logFoodone} alt="logFood" className="img-fluid logFood" />
+                                    <img src={Images.logFoodtwo} alt="logFood" className="img-fluid logFood" />
+                                    <img src={Images.logFoodthree} alt="logFood" className="img-fluid logFood" />
+                                    <span className="logFood">More...</span>
+                                </figure>
+                            </div>
+                        </div>
+                        <div className="col-lg-6">
+                            <div className="logRight verifyIcon mt-5">
+                                <div className="toggleButtonMain">
+                                    <div className="buttonBox">
+                                        <button type="submit" role="button" className="backBtn"><i class="las la-angle-left"></i> Back</button>
+                                    </div>
+                                </div>
+                                <div className="logForm">
+                                    <h6 className="mainHeading mb-3">Verify Email</h6>
+                                    <p className="subHeading mb-4">Enter the verification Code that we just sent your on your
+                                        email address.</p>
+                                    <OtpInput
+                                        value={otp}
+                                        onChange={setOtp}
+                                        numInputs={4}
+                                        // renderSeparator={<span>-</span>}
+                                        renderInput={(props) => <input {...props} className="enterOtp" />}
+                                    />
+                                    <div className="buttonBox mt-5">
+                                        <button type="submit" role="button" className="smallBtn"> Verify</button>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
