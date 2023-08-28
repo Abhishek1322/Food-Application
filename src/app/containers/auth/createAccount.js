d5b6129d964e260a07715b3c2f61438a1de060e9
neha kohli <neha.kohli@itechnolabs.tech>
2023-08-28 19:28:42 +0530

    worked on the forgot password

--- src/app/containers/auth/createAccount.js
+++ src/app/containers/auth/createAccount.js
@@ -11,7 +11,7 @@ const CreateAccount = (props) => {
                 <div className="container-fluid">
                     <div className="row align-items-center">
                         <div className="col-lg-6">
-                            <div className="logleft">
+                            <div className="logleft createAccount">
                                 <figure>
                                     <img src={Images.Logo} alt="logo" className="img-fluid logo" />
                                 </figure>
@@ -42,11 +42,13 @@ const CreateAccount = (props) => {
                                     <h6 className="mainHeading mb-3">Create Account</h6>
                                     <p className="subHeading">Welcome to ServeItLocal</p>
                                 <div className='topInputfields mt-5'>
-                                        <div className='container'>
+                                        <div className='container p-0'>
                                             <div className='row'>
                                                 <div className='col-lg-6'>
-                                                    <input type='text' className='border-input' placeholder='Bangura' />
+                                                  <div className='input-container'>
+  <input type='text' className='border-input' placeholder='Bangura' />
                                                     <label className="border-label">First Name</label>
+                                                  </div>
                                                 </div>
                                                 <div className='col-lg-6'>
                                                     <input type='text' className='border-input' />
@@ -56,13 +58,17 @@ const CreateAccount = (props) => {
                                         </div>
                                     </div>
 
+                                    <div className='col-lg-12' >
                                     <div className="input-container mt-5">
                                         <input type="text" className="border-input" />
                                         <label className="border-label">Email</label>
                                     </div>
+                                    </div>
+                                    <div className='col-lg-12'>
                                     <div className="input-container mt-5">
                                         <input type="password" className="border-input" />
                                         <label className="border-label">Password</label>
+                                    </div>
                                     </div>
                                     <div className="buttonBox mt-5">
                                         <button type="submit" role="button" className="smallBtn"> Login</button>
