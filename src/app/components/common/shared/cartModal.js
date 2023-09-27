5fb3f7f58ad18ad7e5b740358b239a146d02e020
neha kohli <neha.kohli@itechnolabs.tech>
2023-09-27 13:25:18 +0530

    changes

--- src/app/components/common/shared/cartModal.js
+++ src/app/components/common/shared/cartModal.js
@@ -15,7 +15,86 @@ const CartModal = () => {
               <button className='submitOrder_'>Submit</button>
             </div>
        </div> */}
-<div className='modalDetail '>
+        <div className='searchbar '>
+          <input type='text' placeholder='Search Chef near you...' className='searchtext' />
+          <img src={Images.searchbar} className='searchbarImg' alt='searchbar' />
+        </div>
+        <div class="Maincontentbox">
+                <div class="infopages">
+                  <ul class="nav nav-pills mb-2" id="pills-tab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                      <button class="nav-link innertext_ active" id="terms" data-id="1" data-bs-toggle="pill" data-bs-target="#pills-home">New Bookings </button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                      <button class="nav-link innertext_" id="privacy" data-id="2" data-bs-toggle="pill" data-bs-target="#pills-profile">Accepted</button>
+                    </li>
+                  </ul>
+
+                  <div class="tab-content" id="pills-tabContent">
+                    <div class="tab-pane fade active show" id="pills-home">
+                      <div className='container-fluid'>
+                        <div className='row'>
+                          <div className='col-lg-12'>
+                            <div>helllo</div>
+                          </div>
+
+                        </div>
+
+                      </div>
+                    </div>
+                    <div class="tab-pane fade" id="pills-profile">
+                      <div class="tabinnercontent">
+                        <form method="POST" action="https://soulmate.itechnolabs.tech/page" autocomplete="off">
+
+
+                          <h3>Privacy Policy</h3>
+                          <p class="phead">Text Here</p>
+                          <div class="col-md-12 col-12">
+                            <div class="mb-1">
+                              <label class="form-label" for="description">description </label>
+
+
+                            </div>
+                          </div>
+                          <div class="col-12">
+                            <button type="Submit" class="btn btn-primary me-1 waves-effect waves-float waves-light">Submit</button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                    <div class="tab-pane fade" id="pills-contact">
+                      <div class="tabinnercontent">
+                        <form method="POST" action="https://soulmate.itechnolabs.tech/page" autocomplete="off">
+
+                          <p class="phead">Text Here</p>
+                          <div class="col-md-12 col-12">
+                            <div class="mb-1">
+                              <label class="form-label" for="description">description </label>
+
+
+                            </div>
+                          </div>
+                          <div class="col-12">
+                            <button type="Submit" class="btn btn-primary me-1 waves-effect waves-float waves-light">Submit</button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                    <div class="tab-pane fade" id="faq">
+                      <div class="tabinnercontent">
+                        <div class="d-flex justify-content-between">
+                          <h3>FAQ</h3>
+                          <button type="button" class="dt-button create-new btn btn-primary add_question waves-effect waves-float waves-light" data-bs-toggle="modal" data-bs-target="#addFaqModal" data-backdrop="static" data-keyboard="false"><i class="fas fa-plus"></i>&nbsp;&nbsp;Add Faq</button>
+                        </div>
+                        <div class="col-md-12 col-12">
+                          helllo
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        <div className='modalDetail '>
           <div className='usercartDetail'>
             <img src={Images.userProfile} className='userprofile' alt='cartImg' />
             <div className='insideModal'>
@@ -72,7 +151,7 @@ const CartModal = () => {
           </div>
           <button className='orderbutton'>CheckOut</button>
 
-        </div> 
+        </div>
       </div>
 
 
