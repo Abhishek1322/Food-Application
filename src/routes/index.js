c652d5878d83ccb765b96c10d4bed9fbad1b5bfe
Abhishek <abhishek.kumar.rj@itechnolabs.tech>
2023-09-28 19:57:14 +0530

    adding routes check

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import * as Containers from "../app/containers";
 import * as Layouts from "../app/layouts";
-import PrivateCheck from "../app/layouts/PrivateCheck";
+import PrivateChefRoute from "../app/layouts/PrivateChefRoute";
 
 const Router = () => {
   return (
@@ -32,57 +32,48 @@ const Router = () => {
         </Route>
 
         <Route element={<Layouts.AuthLayout />}>
-          <Route element={<PrivateCheck auth={false} />}>
-            <Route path="/choose-roles" element={<Containers.ChooseRoles />} />
-            <Route path="/verification" element={<Containers.Verification />} />
-            <Route
-              path="/create-account/:role"
-              element={<Containers.CreateAccount />}
-            />
-            <Route
-              path="/forgot-password"
-              element={<Containers.ForgotPassword />}
-            />
-            <Route
-              path="/recover-password"
-              element={<Containers.Recoverpassword />}
-            />
-            <Route path="/enter-otp" element={<Containers.EnterOtp />} />
-            <Route
-              path="/change-password"
-              element={<Containers.ChangePassword />}
-            />
-            <Route
-              path="/delete-account"
-              element={<Containers.DeleteAccount />}
-            />
-            <Route path="/loading" element={<Containers.Loading />} />
-            <Route path="/loading-page" element={<Containers.Loadingpage />} />
-            <Route
-              path="/account-deleted"
-              element={<Containers.AccountDeleted />}
-            />
-            <Route
-              path="/term-condition"
-              element={<Containers.TermAndCondition />}
-            />
-            <Route path="/contactu-us" element={<Containers.Contactus />} />
-            <Route
-              path="/privacy-policy"
-              element={<Containers.PrivacyPolicy />}
-            />
-            <Route
-              path="/setup-profile"
-              element={<Containers.SetupProfile />}
-            />
-            <Route path="/cart" element={<Containers.Cart />} />
-            <Route path="/chat" element={<Containers.Chat />} />
-            <Route path="/notification" element={<Containers.Notification />} />
-          </Route>
-
-          <Route element={<PrivateCheck auth={true} />}>
-            {/* <Route path="/setting" element={<Containers.SettingMain />} /> */}
-          </Route>
+          <Route path="/choose-roles" element={<Containers.ChooseRoles />} />
+          <Route path="/verification" element={<Containers.Verification />} />
+          <Route
+            path="/create-account/:role"
+            element={<Containers.CreateAccount />}
+          />
+          <Route
+            path="/forgot-password"
+            element={<Containers.ForgotPassword />}
+          />
+          <Route
+            path="/recover-password"
+            element={<Containers.Recoverpassword />}
+          />
+          <Route path="/enter-otp" element={<Containers.EnterOtp />} />
+          <Route
+            path="/change-password"
+            element={<Containers.ChangePassword />}
+          />
+          <Route
+            path="/delete-account"
+            element={<Containers.DeleteAccount />}
+          />
+          <Route path="/loading" element={<Containers.Loading />} />
+          <Route path="/loading-page" element={<Containers.Loadingpage />} />
+          <Route
+            path="/account-deleted"
+            element={<Containers.AccountDeleted />}
+          />
+          <Route
+            path="/term-condition"
+            element={<Containers.TermAndCondition />}
+          />
+          <Route path="/contactu-us" element={<Containers.Contactus />} />
+          <Route
+            path="/privacy-policy"
+            element={<Containers.PrivacyPolicy />}
+          />
+          <Route path="/setup-profile" element={<Containers.SetupProfile />} />
+          <Route path="/cart" element={<Containers.Cart />} />
+          <Route path="/chat" element={<Containers.Chat />} />
+          <Route path="/notification" element={<Containers.Notification />} />
         </Route>
       </Routes>
     </>
