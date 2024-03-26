3639c769d686fe1e31df0d73fe261d3759451794
Abhishek <abhishek.kumar.rj@itechnolabs.tech>
2024-03-26 19:50:13 +0530

    changes

--- src/app/components/common/shared/CartFoodModalOrder.js
+++ src/app/components/common/shared/CartFoodModalOrder.js
@@ -288,9 +288,9 @@ const CartFoodModalOrder = ({ menuId, close }) => {
                   type="button"
                 >
                   Order Now
-                  {/* {userData?.loading && isLoading === "order" && ( */}
+                  {userData?.loading && isLoading === "order" && (
                     <span className="spinner-border spinner-border-sm ms-1"></span>
-                  {/* )} */}
+                  )}
                 </button>
                 <button
                   disabled={userData?.loading}
