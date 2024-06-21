8d490ddaebc655c4aa7ded9622cc3fd8e26a2a58
Abhishek <abhishek.kumar.rj@itechnolabs.tech>
2024-06-21 11:06:20 +0530

    change env variables

--- src/constants/baseUrl.js
+++ src/constants/baseUrl.js
@@ -1,11 +1,7 @@
-
 const BaseUrl = {
-    API_URL: 'https://dev-api.serveitlocal.itechnolabs.tech/',
-    // PAID
-    // GEO_CODE_API_URL: 'https://api.opencagedata.com/geocode/v1/',
-    // FREE
-    // GET_LOCATION_API_URL:"https://nominatim.openstreetmap.org/"
-    GET_LOCATION_API_URL_GOOGLE_MAPS:"https://maps.googleapis.com/"
-}
+  API_URL: process.env.REACT_APP_BACKEND_API_URL,
+  GET_LOCATION_API_URL_GOOGLE_MAPS:
+    process.env.REACT_APP_GET_LOCATION_API_URL_GOOGLE_MAPS,
+};
 
 export default BaseUrl;
